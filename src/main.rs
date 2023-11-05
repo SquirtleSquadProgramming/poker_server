ccf78e621176517998ab281080dcc4715a22c8fa
Elle Fleming <39723774+s-0fia@users.noreply.github.com>
2023-11-05 17:06:58 +1300

    setup basic axum server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(index_handler));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 80));
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn index_handler() -> &'static str {
+    "Hello, world!"
 }
